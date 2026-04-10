01100f622d2a53e3b648426c7877beb7507e3268
Sergey Bolotnikov <sbolotnikov@gmail.com>
2026-04-10 02:04:52 -0400

    standalone and image to share

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,31 @@
 import type { Metadata } from 'next';
 import './globals.css';
 
+ 
 export const metadata: Metadata = {
-  title: 'Audio Processor',
-  description: 'Loudness Normalization & Editing',
+  manifest: '/site.webmanifest',
+  metadataBase: new URL(process.env.NEXTAUTH_URL + ''),
+  title: {
+    template: '%s',
+    default: 'Audio Processor | Web Application for Audio Processing',
+  },
+  keywords:"audio processing, loudness normalization, audio editing, web application",
+  description:
+    'Loudness Normalization & Editing',
+  openGraph: {
+    title: {
+      template: '%s',
+      default: 'Audio Processor | Web Application for Audio Processing',
+    },
+    description:
+      'Loudness Normalization & Editing',
+    url: process.env.NEXTAUTH_URL + '',
+    type: 'website',
+    images: [
+      { url: process.env.NEXTAUTH_URL + '/logo2.jpg', width: 1200, height: 640 },
+    ],
+  },
 };
-
 export default function RootLayout({
   children,
 }: {
@@ -13,7 +33,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className="antialiased">{children}</body>
+      <link
+  rel="icon"
+  href="/icon?<generated>"
+  type="image/<generated>"
+  sizes="<generated>"
+/>
+<link
+  rel="apple-touch-icon"
+  href="/apple-icon?<generated>"
+  type="image/<generated>"
+  sizes="<generated>"
+/> 
+
+      <body suppressHydrationWarning={true} className="antialiased">{children}</body>
     </html>
   );
 }
